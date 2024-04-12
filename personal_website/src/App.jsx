9deeadd950963bba238735a45db1cc267d47fa7b
Christopher Tremblay <christopher.tremblay@epfl.ch>
2024-04-12 13:53:19 +0200

    Initial push after vite init

---
+++ personal_website/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>The start of something great</div>
+    </>
+  );
+}
+
+export default App;
