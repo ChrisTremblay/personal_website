4bb0b4cc539af9e202cdaba1394e872d4e39fdcc
Christopher Tremblay <christopher.tremblay@epfl.ch>
2024-04-18 16:06:41 +0200

    Dark and light mode + color modes

--- personal_website/src/App.jsx
+++ personal_website/src/App.jsx
@@ -1,10 +1,18 @@
+import { useEffect } from 'react';
 import { Footer } from './components/Footer';
 import { NavigationBar } from './components/NavigationBar/NavigationBar';
 const App = () => {
+  useEffect(() => {
+    document.body.setAttribute('data-mode', 'light-mode');
+    document.body.setAttribute('data-color', 'blue');
+  }, []);
+
   return (
     <>
       <NavigationBar />
-      <div>The start of something great</div>
+      <div style={{ backgroundColor: 'black' }}>
+        The start of something great
+      </div>
       <Footer />
     </>
   );
