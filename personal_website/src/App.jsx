ab03e1dabb9e45494721af81f06e9bae97b9a06d
Christopher Tremblay <christopher.tremblay@epfl.ch>
2024-04-18 12:10:05 +0200

    New logo version with reusable

--- personal_website/src/App.jsx
+++ personal_website/src/App.jsx
@@ -1,11 +1,10 @@
-import Container from 'react-bootstrap/Container';
 import { Footer } from './components/Footer';
 import { NavigationBar } from './components/NavigationBar/NavigationBar';
 const App = () => {
   return (
     <>
       <NavigationBar />
-      <Container>The start of something great</Container>
+      <div>The start of something great</div>
       <Footer />
     </>
   );
--- personal_website/src/components/NavigationBar/NavigationBar.jsx
+++ personal_website/src/components/NavigationBar/NavigationBar.jsx
@@ -1,43 +1,12 @@
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
 import './NavigationBar.css';
 import { Logo } from '../Logo/Logo';
 
 export const NavigationBar = () => {
   return (
-    <>
-      <Navbar collapseOnSelect expand='lg' className='bg-body-tertiary'>
-        <Container>
-          <Navbar.Brand href='#home'>
-            <Logo
-              strokeColor={'#dfdfdf'}
-              viewBox={1100}
-              height={40}
-              width={40}
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id='responsive-navbar-nav'>
-            <Nav
-              className='me-auto'
-              activeKey='/home'
-              onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-            >
-              <Nav.Item>
-                <Nav.Link eventKey='#coding'>Coding</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey='#art'>Art</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey='#sport'>Sport</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+    <div>
+      <nav>
+        <Logo theme='dark' color='teal' />
+      </nav>
+    </div>
   );
 };
